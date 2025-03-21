59195c0221a763bd0f6c5191e959f136d2c4ae09
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-03-21 17:15:48 -0500

    feat(markdown-tables): WIP

--- lua/custom/dev.lua
+++ lua/custom/dev.lua
@@ -1,3 +1,19 @@
 local M = {}
 
+-- vim.ui.input({ prompt = "Hello? " }, function(input)
+--     if input then
+--         vim.print(input)
+--     end
+-- end)
+--
+vim.cmd("vsplit +term")
+vim.cmd("split +term")
+vim.print(vim.api.nvim_list_bufs())
+vim.print(vim.api.nvim_list_wins())
+
+for _, value in pairs(vim.api.nvim_list_chans()) do
+    vim.print(value.id)
+    vim.print(value.mode)
+end
+
 return M
