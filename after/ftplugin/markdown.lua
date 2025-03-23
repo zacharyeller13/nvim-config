ef74d9219e70faf9f71b2dd8c5d216862ad12ed9
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-03-23 18:44:28 -0500

    fix(markdown): Comments and buffer-only mapping

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,16 +1,12 @@
 ---Replaces `[toc]` in a markdown file
----@param args table table of arguments
-local function replace_toc(args)
+local function replace_toc(_)
     -- TOC probably never more than 10 lines down
-    -- print(vim.inspect(args))
     local lines = vim.api.nvim_buf_get_lines(0, 0, 10, false)
 
     for idx, line in pairs(lines) do
-        -- print(key, value)
         if line == '[toc]' then
             vim.api.nvim_buf_set_lines(0, idx - 1, idx, false, { '<!-- mtoc-start -->', '<!-- mtoc-end -->' })
             vim.cmd.Mtoc()
-            -- Early return a little bit
             return
         end
     end
@@ -18,15 +14,13 @@ end
 
 ---Formats a markdown table in visual mode and then returns to normal mode
 ---Thanks to https://heitorpb.github.io/bla/format-tables-in-vim/
-local function format_table()
-    local start = vim.fn.getpos('v')
-    vim.print('start: ', start)
-    local end_idx = vim.fn.getpos('.')
-    vim.print('end: ', end_idx)
-    vim.api.nvim_cmd(
-        { range = { start[2], end_idx[2] }, cmd = '!', args = { 'column', '-t', '-s', "'|'", '-o', "'|'" } },
-        {}
-    )
+---for the explanation of using `tr` and `column`
+---@param args table? A table of user-command arguments if called from a user-command
+local function format_table(args)
+    local start = args and args.line1 or vim.fn.getpos('v')[2]
+    local end_idx = args and args.line2 or vim.fn.getpos('.')[2]
+    vim.api.nvim_cmd({ range = { start, end_idx }, cmd = '!', args = { "tr -s ' ' | column -t -s '|' -o '|'" } }, {})
+
     local key = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
     vim.api.nvim_feedkeys(key, 'v', false)
 end
@@ -38,6 +32,8 @@ vim.api.nvim_buf_create_user_command(
     { desc = "Finds and replaces '[toc]' in markdown file" }
 )
 
+vim.api.nvim_buf_create_user_command(0, 'FormatTable', format_table, { desc = 'Formats markdown table', range = true })
+
 -- Kemaps
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true, buffer = true, desc = 'Markdown TOC' }
@@ -45,4 +41,5 @@ map('n', '<leader>md', vim.cmd.Mtoc, opts)
 opts = vim.tbl_extend('force', opts, { desc = 'Replace [TOC]' })
 map('n', '<leader>toc', vim.cmd.ReplaceToc, opts)
 
-map('v', '<leader>f', format_table, { desc = '[F]ormat markdown table' })
+--
+map('v', '<leader>f', format_table, { buffer = true, desc = '[F]ormat markdown table' })
