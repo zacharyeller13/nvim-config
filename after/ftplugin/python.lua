dfeab990f60aa7c7040484874dfd35e7f80091f5
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-04-07 13:01:43 -0500

    feat(terminal-state): Functional, toggle-able terminal buffer with basic python keymaps

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,13 +1,23 @@
 -- Run python code in a new terminal
 -- vim.keymap.set('n', '<space><space>x', '<cmd>term python3 %<CR>')
 
-local repl = require('custom.dev')
+local repl = require("custom.dev")
 
-local com = "print('hello world')\n"
+repl.callback = function()
+    repl:send("uv run python\n")
+end
 
-vim.keymap.set('n', '<space><space>t', function()
+local com = "     print('hello world')\n"
+
+vim.keymap.set("n", "<space><space>t", function()
     repl:create_term()
-end)
-vim.keymap.set('n', '<space><space>x', function()
+end, { buffer = true })
+
+vim.keymap.set("n", "<space><space>x", function()
+    com, _ = com:gsub("^ +", "")
     repl:send(com)
+end, { buffer = true })
+
+vim.keymap.set("n", "<space>t", function()
+    repl:toggle()
 end)
