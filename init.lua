599e8993da8a6269c1da432cfa0f83723f9e76cb
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-04-24 09:23:13 -0500

    feat(wezterm-types): Add wezterm-types, more up to date version from gonstoll
    Update a few packages as well

--- init.lua
+++ init.lua
@@ -301,11 +301,15 @@ require("lazy").setup({
     {
         "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
+        dependencies = {
+            { "gonstoll/wezterm-types", lazy = true },
+        },
         opts = {
             library = {
                 -- See the configuration section for more details
                 -- Load luvit types when the `vim.uv` word is found
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
             },
         },
     },
