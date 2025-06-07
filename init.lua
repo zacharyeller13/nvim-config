04f1c25338cd93a19b8b5e7783899d8d72e0e964
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-06-06 22:43:30 -0500

    feat(roslyn): Remove csharp_ls

--- init.lua
+++ init.lua
@@ -369,9 +369,6 @@ require("lazy").setup({
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
                 callback = function(event)
-                    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-                    -- to define small helper and utility functions so you don't have to repeat yourself
-                    -- many times.
                     --
                     -- In this case, we create a function that lets us more easily define mappings specific
                     -- for LSP related items. It sets the mode, buffer and description for us each time.
@@ -427,8 +424,8 @@ require("lazy").setup({
                     -- The following two autocommands are used to highlight references of the
                     -- word under your cursor when your cursor rests there for a little while.
                     --    See `:help CursorHold` for information about when this is executed
-                    --
                     -- When you move your cursor, the highlights will be cleared (the second autocommand).
+
                     ---@type vim.lsp.Client?
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     if
@@ -585,7 +582,7 @@ require("lazy").setup({
                 },
             }
 
-            -- Only install csharp_ls if dotnet is installed
+            -- Only install csharp_ls/roslyn if dotnet is installed
             if vim.fn.executable("dotnet") == 1 then
                 servers = vim.tbl_deep_extend("force", servers, {
                     -- csharp_ls = {},
@@ -624,12 +621,12 @@ require("lazy").setup({
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
                 "stylua", -- Used to format lua code
-                "ruff",
                 "xmlformatter",
                 "sql-formatter",
             })
             require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
+            -- ensure installed and automatic enable are defaults
             require("mason-lspconfig").setup({
                 handlers = {
                     function(server_name)
@@ -639,6 +636,7 @@ require("lazy").setup({
                         -- certain features of an LSP (for example, turning off formatting for tsserver)
                         server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
                         require("lspconfig")[server_name].setup(server)
+                        -- vim.lsp.config(server_name, server)
                     end,
                 },
             })
