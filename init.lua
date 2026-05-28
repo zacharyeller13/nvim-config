7d78d14d9ce25be2ec6a38562a89e435727594d6
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-05-28 09:22:01 -0500

    refactor(git):  Move gitsigns to own module and add vim-fugitive to it

--- init.lua
+++ init.lua
@@ -106,24 +106,6 @@ require("lazy").setup({
     -- "gc" to comment visual regions/lines
     { "numToStr/Comment.nvim", opts = {} },
 
-    -- Here is a more advanced example where we pass configuration
-    -- options to `gitsigns.nvim`. This is equivalent to the following lua:
-    --    require('gitsigns').setup({ ... })
-    --
-    -- See `:help gitsigns` to understand what the configuration keys do
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        "lewis6991/gitsigns.nvim",
-        opts = {
-            signs = {
-                add = { text = "+" },
-                change = { text = "~" },
-                delete = { text = "_" },
-                topdelete = { text = "‾" },
-                changedelete = { text = "~" },
-            },
-        },
-    },
-
     -- NOTE: Plugins can also be configured to run lua code when they are loaded.
     --
     -- This is often very useful to both group configuration, as well as handle
@@ -264,7 +246,7 @@ require("lazy").setup({
             end
 
             -- For mini.files
-            require("mini.icons").setup()
+            -- require("mini.icons").setup()
             -- require("mini.files").setup()
 
             -- ... and there is more!
