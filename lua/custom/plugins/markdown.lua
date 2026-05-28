43085c39669c9df1c7282840b661c6dcc5c8fea4
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-05-28 12:29:07 -0500

    refactor(markdwon): Move markdown related plugins to single module

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "hedyhli/markdown-toc.nvim",
+        ft = "markdown", -- Lazy load on markdown filetype
+        cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+        opts = {
+            auto_update = true,
+        },
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = { file_types = { "markdown", "codecompanion", "joplin.markdown" } },
+        ft = { "markdown", "codecompanion", "joplin.markdown" },
+    },
+}
