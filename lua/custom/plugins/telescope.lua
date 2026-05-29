0ab651a7b30917c7677416e16d0db1001029578a
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-05-29 10:16:01 -0500

    refactor(telescope): Only log fzf/ui-select if they fail to load

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -102,10 +102,12 @@ local M = { -- Fuzzy Finder (files, lsp, etc)
         })
 
         -- Enable telescope extensions, if they are installed
-        if pcall(require("telescope").load_extension, "fzf") then
-            print("started fzf")
+        if not pcall(require("telescope").load_extension, "fzf") then
+            vim.notify("[Telescope] fzf not found", vim.log.levels.ERROR)
         end
-        pcall(require("telescope").load_extension, "ui-select")
+        if not pcall(require("telescope").load_extension, "ui-select") then
+            vim.notify("[Telescope] ui-select not found", vim.log.levels.ERROR)
+        end
 
         -- See `:help telescope.builtin`
         local builtin = require("telescope.builtin")
@@ -119,6 +121,7 @@ local M = { -- Fuzzy Finder (files, lsp, etc)
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "[G]it [S]tatus" })
 
         -- Slightly advanced example of overriding default behavior and theme
         vim.keymap.set("n", "<leader>/", function()
