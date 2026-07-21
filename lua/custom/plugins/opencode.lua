e19f9d978a8bcbbc883dbad18253b2104e9b230d
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-07-21 10:09:32 -0500

    refactor(opencode.nvim): remove unused options

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -3,8 +3,8 @@ return {
     dependencies = {
         -- Recommended for `ask()` and `select()`.
         -- Required for `snacks` provider.
-        ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-        { "folke/snacks.nvim", lazy = false, priority = 1000, opts = { input = {}, picker = {}, terminal = {} } },
+        ---@module 'snacks'
+        { "folke/snacks.nvim", lazy = false, priority = 1000, opts = { input = {}, picker = {} } },
     },
     version = "*",
     config = function()
@@ -40,13 +40,6 @@ return {
         vim.keymap.set("n", "goo", function()
             return require("opencode").operator("@this ") .. "_"
         end, { desc = "Add line to opencode", expr = true })
-
-        vim.keymap.set("n", "<S-C-u>", function()
-            require("opencode").command("session.half.page.up")
-        end, { desc = "Scroll opencode up" })
-        vim.keymap.set("n", "<S-C-d>", function()
-            require("opencode").command("session.half.page.down")
-        end, { desc = "Scroll opencode down" })
 
         -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o…".
         vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
