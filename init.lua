3a8da891483de04940fb4aed969a2f5211db7575
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-02 12:05:32 -0500

    refactor(deprecations): highlight yank change in v0.13, vim.loop -> vim.uv

--- init.lua
+++ init.lua
@@ -69,16 +69,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
-        vim.hl.on_yank()
-        -- deprecated
-        -- vim.highlight.on_yank()
+        if vim.fn.has("nvim-0.13") == 1 then
+            vim.hl.hl_op()
+        else
+            vim.hl.on_yank()
+        end
     end,
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
