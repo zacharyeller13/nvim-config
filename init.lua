ad0e8abc1f096709222f2f47cf495494fe3677f9
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-03 20:36:12 -0500

    fix(provider-health): disable unused python3 provider

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable unused providers
--- vim.g.loaded_python3_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
